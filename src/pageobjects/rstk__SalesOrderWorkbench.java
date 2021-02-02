76cb8c2b848c4eaf542092d9cc043655d77dd8fe
achoudhary5501 <62293526+achoudhary5501@users.noreply.github.com>
2021-02-02 20:50:23 +0530

    commit

--- src/pageobjects/rstk__SalesOrderWorkbench.java
+++ src/pageobjects/rstk__SalesOrderWorkbench.java
@@ -19,5 +19,24 @@ public class rstk__SalesOrderWorkbench {
 	@TextType()
 	@VisualforceBy(componentXPath = "c:standardHeader//h2[contains(@class, \"pageDescription\")]")
 	public WebElement pageTitle;
+	@BooleanType()
+	@FindBy(id = "pg:fm:pb:j_id269:j_id280:soiss_createinvoice__c")
+	public WebElement soiss_createinvoice__c;
+	@ButtonType()
+	@FindByLabel(label = "Process Selected Lines")
+	public WebElement processSelectedLines;
+	@PageRow()
+	public static class Table {
+
+		@TextType()
+		@FindByLabel(label = "Qty Shipped")
+		public WebElement qtyShipped;
+		@LinkType()
+		@FindBy(xpath = "//span[contains(@id,'soship_invoice__c')]")
+		public WebElement invoiceNumber;
+	}
+	@FindBy(id = "pg:fm:pb:j_id297:pbt:tb")
+	@PageTable(firstRowContainsHeaders = false, row = Table.class)
+	public List<Table> table;
 	
 }
